8a0baa1b9d4ac9bd37741a8a51ac45c51a21562f
Adam Vessey <adam-vessey@users.noreply.github.com>
2016-06-22 12:43:15 -0300

    Rework remote tests to avoid issues.

--- trippi-sail-blazegraph-remote/src/test/java/ca/discoverygarden/trippi/sesame/blazegraph/RemoteBlazegraphViaSparqlIntegrationTest.java
+++ trippi-sail-blazegraph-remote/src/test/java/ca/discoverygarden/trippi/sesame/blazegraph/RemoteBlazegraphViaSparqlIntegrationTest.java
@@ -3,6 +3,6 @@ package ca.discoverygarden.trippi.sesame.blazegraph;
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration
-public class RemoteBlazegraphViaSparqlIntegrationTest extends RemoteBlazegraphIntegrationTest {
-	// No-op; everything is in RemoteBlazegraphIntegrationTest.
+public class RemoteBlazegraphViaSparqlIntegrationTest extends AbstractRemoteBlazegraphIntegrationTest {
+	// No-op; everything is in AbstractRemoteBlazegraphIntegrationTest.
 }
